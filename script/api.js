6334a79fcdc152ebb2651a3567f4f9277469ea62
Clark <clark.dumon@gmail.com>
2024-08-27 16:57:13 +0800

    pending HTML and CSS

--- script/api.js
+++ script/api.js
@@ -22,42 +22,85 @@ async function apiFetcher(url,funcName) {
 
 
 //This is to check 
-async function getPokemon(searchValue){
-    searchValue = searchValue.toLowerCase()
+async function searchPokemon(searchValue) {
+    searchValue = searchValue.toLowerCase();
     //Checks if search value is not null or empty // used ternary operator
-    searchValue = searchValue!=null?searchValue:'';
-   
+    searchValue = searchValue != null ? searchValue : "";
 
-    let data = await  apiFetcher(POKEAPI,'getPokemon')
-    let pokeNames =    data.results.map(res=>res.name)
-    let pokemons = pokeNames.filter(name=>{
-        if(name.includes(searchValue)){
-            return name;
-        }
-    })
-    
-    const pokeDex = await []
-    const pokeMan = pokemons.forEach(async pokemon => {
+    let getList = await apiFetcher(POKEAPI, "searchPokemon");
+    let pokeNames = getList.results.map((res) => res.name);
+    // console.log(pokeNames);
+
+    let pokemonDetails = (pokeNames)=>{
+        console.log(pokeNames)
+        let pokeDex = [];
+        pokeNames.forEach( pokemon => {
+            pokeDex.push(apiFetcher(POKEMONPATH+pokemon,"PokemonForEach"))
+        });
         
-        const data = await apiFetcher(POKEMONPATH+pokemon,"PokemonsForEach")
-
-        if(data.is_default){
-                const poke ={
-                    name:data.name
-
+        Promise.all(pokeDex).then(async result=>{
+            const pokemon = result.map(data=>{
+                let details = {
+                    is_default: data.is_default,
+                    name: data.name,
+                    sprite:data.sprites.other["official-artwork"].front_default,
+                    type: data.types.map(type=>type.type.name)
+                };
+                return details
+            })
+            return pokemon
+        }).then(pokemon=>{
+            let thedefault = pokemon.filter(data=>data.is_default)
+            return thedefault
+        }).then(pokemon=>{
+            pokemon.forEach(name=>{
+                if(name.name.includes(searchValue)){
+                    console.log(name)
                 }
-                return
-                
-            
-        }
-        
-        
-    });
-    
-    
-}
+            })
+        })
 
-getPokemon('saur')
+    };pokemonDetails(pokeNames);
+    
+    // let  sss = pokeNames.forEach( async eachPokemon => {
+    //     const data = await apiFetcher(POKEMONPATH+eachPokemon,"PokemonsForEach")
+    //     console.log(data);
+    //     return data
+    // })//let pokemonDetails
+    
+
+   
+    
+}//async function getPokemon(searchValue)
+
+
+
+
+searchPokemon('dori')
 apiFetcher(POKEAPI,'test')
 
 
+// let pokemons = pokeNames.filter(name=>{
+//         if(name.includes(searchValue)){
+//             return name;
+//         }
+//     })//let pokemons 
+    
+//     const pokeDex = await []
+//     const pokeList = pokemons.forEach(async pokemon => {
+//         const data = await apiFetcher(POKEMONPATH+pokemon,"PokemonsForEach")
+//         console.log(data)
+//         if(data.is_default){
+//             const details ={
+
+//                 name:data.name,
+//                 sprite: data.sprites.other["official-artwork"].front_default,
+//                 type:data.types.map(types=>{
+//                     return types.type.name
+//                 }),//type:data.types.map
+//             }//const details
+//             pokeDex.push(details) ;// details
+//         }//ending-IF
+        
+//     });//const pokeList
+//     console.log( pokeDex)
